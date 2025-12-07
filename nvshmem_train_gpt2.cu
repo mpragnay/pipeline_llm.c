b05bce28764cd7fb250bdbb7dc692e22bb23b1f7
Raju Kanumuri <vk2646@nyu.edu>
2025-12-06 23:47:13 -0500

    fix 9

--- nvshmem_train_gpt2.cu
+++ nvshmem_train_gpt2.cu
@@ -2094,8 +2094,7 @@ int main(int argc, char *argv[]) {
   // TF32 precision is equivalent to
   // torch.set_float32_matmul_precision('high')
   int enable_tf32 = deviceProp.major >= 8 ? 1 : 0;
-  cublas_compute_type =
-      enable_tf32 ? CUBLAS_COMPUTE_32F_FAST_TF32 : CUBLAS_COMPUTE_32F;
+  // Note: cublas_compute_type was removed as it was unused
   cublasMath_t cublas_math_mode =
       enable_tf32 ? CUBLAS_TF32_TENSOR_OP_MATH : CUBLAS_DEFAULT_MATH;
   cublasCheck(cublasSetMathMode(cublas_handle, cublas_math_mode));
